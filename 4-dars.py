c53e12e8c7a69519bbf2ef03cec88829373cfdde
Qodirqulov Muhammadjon <muhammadjonqodirqulov25@gmail.com>
2021-07-04 22:20:58 +0500

    4-dars
    4-darsning uyga vazifalari(python.sariq.dev)

---
+++ 4-dars.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  4 21:26:55 2021
+
+@author: Muhammadjon
+"""
+
+
+dars = 4
+
+
+
+a = 'Hello World'
+print(a)
+xabar = 'MN'
+print(xabar)
+xabar = 'MN25'
+print(xabar)
+
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
