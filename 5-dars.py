2607c2bc63ab916e5468d6f376acd2ce446879f1
Qodirqulov Muhammadjon <muhammadjonqodirqulov25@gmail.com>
2021-07-05 22:06:07 +0500

    5-dars
    5-dars uyga vazifa(python.sariq.dev)

---
+++ 5-dars.py
@@ -0,0 +1,21 @@
+
+
+
+dars = 4
+
+
+kocha = 'Bog`bon'
+mahalla = 'Sog`bon'
+tuman = 'Bodomzor'
+viloyat = 'Samarqand'
+
+
+
+print('Quyidagi ma`lumotlarni kiritsangiz')
+
+kocha = input('Ko`changiz: ')
+mahalla = input('Mahallangiz: ')
+tuman = input('Tumaningiz: ')
+viloyat = input('Viloyatingiz: ')
+manzil = f'{kocha} ko`chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati'
+print('Siz ' + manzil + 'da istiqomat qilasiz')
