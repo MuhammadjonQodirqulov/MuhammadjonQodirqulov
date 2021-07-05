146cc6a25d5e94e4f22e8e59172589be73dfe27b
Qodirqulov Muhammadjon <muhammadjonqodirqulov25@gmail.com>
2021-07-05 22:31:54 +0500

    6-dars
    6-dars uyga vazifa(python.sariq.dev)

---
+++ 6-dars.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  5 22:11:53 2021
+
+@author: Muhammadjon
+"""
+
+
+
+dars = 6
+
+
+son = int(input('Istalgan son kiriting: '))
+print('Sonning kvadrati ' + str(son**2) + 'ga teng')
+print('Sonning kubi ' + str(son**3) + 'ga teng')
+
+yosh = str(input('Yoshingizni kiriting: '))
+ty = 2021 - int(yosh)
+
+print('Siz ' + str(ty) + '-yil tug`ilgansiz')
+
+son1 = float(input
+           ('Birinchi sonni kirgazing: ') )
+son2 = float(input
+           ('Ikkinchi sonni kirgazing: ')
+           )
+a = son1 + son2
+b = son1 - son2
+c = son1 * son2
+d = son1 / son2
+
+print('Yig`indi ' + str(a) + 'ga teng')
+print('Ayirma ' + str(a) + 'ga teng')
+print('Ko`paytma ' + str(a) + 'ga teng')
+print('Bo`linma ' + str(a) + 'ga teng')
